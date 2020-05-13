e301537fa7424e4a5a2065754fe5123143586e10
punam <punamjasud@gmail.com>
2020-05-13 09:20:11 +0530

    Largest and Smmalllest Number in Unsorted Array

---
+++ Data_Strctures_Problem/src/Array_Problems/LargestAndSmallestInUnsortedArray.java
@@ -0,0 +1,38 @@
+package Array_Problems;
+
+import java.util.Scanner;
+
+public class LargestAndSmallestInUnsortedArray {
+
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("How many ELements : ");
+		int n = sc.nextInt();
+		
+		int a[] = new int[n];
+		System.out.println("Enter Elements: ");
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+ 
+		largestAndSmallest(a);
+	}
+
+	private static void largestAndSmallest(int[] a) {
+		int largest,smallest;
+		largest = smallest = a[0];
+		for (int i = 1; i < a.length; i++) {
+			if(a[i] < smallest)
+			{
+				smallest = a[i];
+			}
+			else if(a[i] > largest)
+			{
+				largest = a[i];
+			}
+		}
+		System.out.println("Largest : " + largest + " Smallest : " + smallest);
+		
+	}
+
+}
